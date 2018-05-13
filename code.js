de2c31b8b5070b28f86e4866f0b388db085280b8
NuclearPhone <subbydacodah@gmail.com>
2018-05-12 21:13:37 -0400

    added and fixed privileged users

--- code.js
+++ code.js
@@ -62,8 +62,8 @@ client.on("message", (message) =>
 		console.log(message.content + " " + message.author.id);
 	}
 	//change presence
-	else if (message.content.startsWith(config.prefix + "presence"))
-	{
+	else if (message.content.startsWith(config.prefix + "presence") && config.privilegedusers.includes(message.author.id) == true)
+	{	
 		client.user.setPresence({game: { name: ' ' + args.join(" ")}, status: 'online' });
 		message.channel.send("Presence has been changed to: " + args.join(" "));
 		console.log(message.content + " " + args + "    " + message.author.id);
